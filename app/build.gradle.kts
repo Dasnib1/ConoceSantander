0b2540fa5f277b3c924ad2408e37a447475fd228
ivanmanso31 <thecrazyplayerlol@gmail.com>
2024-02-13 13:10:37 +0100

    permisos conseguidos yuju

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation ("com.google.maps.android:maps-compose:2.11.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     //implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("com.google.accompanist:accompanist-permissions:0.35.0-alpha")
 
 
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/example/conocesantander/MainActivity.kt
+++ app/src/main/java/com/example/conocesantander/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.conocesantander
 
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import com.example.compose.ConoceSantanderTheme
 import com.example.conocesantander.ui.ConoceSantanderApp
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 
 
 class MainActivity : ComponentActivity() {
@@ -17,15 +32,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var darkTheme by rememberSaveable { mutableStateOf(false)}
+            var darkTheme by rememberSaveable { mutableStateOf(false) }
+            var locationPermissionGranted by rememberSaveable { mutableStateOf(false) }
 
-            ConoceSantanderTheme (darkTheme = darkTheme) {
+            ConoceSantanderTheme(darkTheme = darkTheme) {
+                FeatureThatRequiresCameraPermission()
+
+                // Resto de tu UI aquí
                 ConoceSantanderApp(
                     darkTheme = darkTheme,
                     onThemeUpdated = { updatedTheme ->
-                        darkTheme = updatedTheme
+                        //darkTheme = updatedTheme
                     }
                 )
+            }
+        }
+    }
+
+    @OptIn(ExperimentalPermissionsApi::class)
+    @Composable
+    private fun FeatureThatRequiresCameraPermission() {
+
+        // Camera permission state
+        val cameraPermissionState = rememberPermissionState(
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        if (cameraPermissionState.status.isGranted) {
+            Text("Camera permission Granted")
+        } else {
+            Column {
+                val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+                    // If the user has denied the permission but the rationale can be shown,
+                    // then gently explain why the app requires this permission
+                    "The camera is important for this app. Please grant the permission."
+                } else {
+                    // If it's the first time the user lands on this feature, or the user
+                    // doesn't want to be asked again for this permission, explain that the
+                    // permission is required
+                    "Camera permission required for this feature to be available. " +
+                            "Please grant the permission"
+                }
+                Text(textToShow)
+                Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                    Text("Request permission")
+                }
             }
         }
     }
