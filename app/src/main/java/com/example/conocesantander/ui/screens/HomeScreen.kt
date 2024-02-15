0a9b03640552af9a9d5ff96e20a5ae29f24cca88
Dasnib1 <daniel.dawdam@gmail.com>
2024-02-15 12:45:01 +0100

    Commit obligado para pull

--- app/src/main/java/com/example/conocesantander/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/conocesantander/ui/screens/HomeScreen.kt
@@ -4,15 +4,40 @@ import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.location.Location
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.conocesantander.BuildConfig
+import com.example.conocesantander.R
 import com.example.conocesantander.ui.classes.NearbySearchResponse
 import com.example.conocesantander.ui.classes.PlacesClient.create
 import com.example.conocesantander.ui.classes.Restaurant
@@ -149,4 +174,110 @@ fun encuentra(placesClient: PlacesClient){
     Text(text = lugarDireccion)
 
 
+}
+
+@Composable
+fun RestaurantRecommendationCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = Color(0xFFE6EE9C))
+                .padding(16.dp)
+        ) {
+            Column {
+                Text(
+                    text = "Recomendaciones de Restaurantes",
+                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                //recommendationList.forEach { recommendation ->
+
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    RestaurantCard()
+
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                //}
+
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    //recommendationList.forEach { recommendation ->
+                    RestaurantCard()
+                    //}
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    //recommendationList.forEach { recommendation ->
+                    RestaurantCard()
+                    //}
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun RestaurantCard() {
+    Card(
+        modifier = Modifier
+            .padding(end = 8.dp)
+            .height(100.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(100.dp)
+                    .fillMaxHeight()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "recommendation.name",
+                    style = TextStyle(fontSize = 14.sp, fontWeight = FontWeight.Bold)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = "recommendation.address",
+                    style = TextStyle(fontSize = 12.sp)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = "recommendation.rating".toString(),
+                    style = TextStyle(fontSize = 12.sp)
+                )
+
+            }
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = null,
+                tint = Color.Red,
+                modifier = Modifier.size(16.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+        }
+    }
 }
